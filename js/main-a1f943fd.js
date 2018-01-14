e7f4f65c496d05901b36ed3cd96f38713e13e141
yaorong <yaorong@yaorongs-mbp.speedport_w_724v_typ_a_05011603_05_028>
2018-01-14 23:55:34 +0100

    Updates

---
+++ js/main-a1f943fd.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function(){
+  $(document).on("scroll", onScroll);
+  
+  
+
+  //forzen nav
+    var nav = $(".nav-top");
+    $(window).on("scroll", function(e) {  
+      if ($(this).scrollTop() > 100) {
+        nav.addClass("fix-nav");
+      } else {
+        nav.removeClass("fix-nav");
+      }
+    });
+
+  //smooth scroll
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
+
+  //active nav item
+  function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#nav nav div a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#nav nav div a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+      });
+    }
+    
+});
