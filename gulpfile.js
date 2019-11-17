898ef7334f58c6fc7eb6a1edb6b4fe391d5c3b08
yaorong <yaorong@yaorongs-MacBook-Pro.local>
2019-11-17 22:35:09 +0100

    gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp         = require("gulp"),
     del          = require("del")
 
 // Compile SCSS files to CSS
-gulp.task("scss", function () {
+gulp.task("scss", function (done) {
 
     //Delete our old css files
     del(["static/css/**/*"])
@@ -25,31 +25,44 @@ gulp.task("scss", function () {
         .pipe(hash.manifest("hash.json"))
         //Put the map in the data directory
         .pipe(gulp.dest("data/css"))
-})
+
+        done();
+});
 
 // Hash images
-gulp.task("images", function () {  
+gulp.task("images", function (done) {  
     del(["static/img/**/*"])
     gulp.src("src/img/**/*")
         .pipe(gulp.dest("static/img"))
-})
+
+     done();
+});
 
 // Hash javascript
-gulp.task("js", function () {
+gulp.task("js", function (done) {
     del(["static/js/**/*"])
     gulp.src("src/js/**/*")
         .pipe(hash())
         .pipe(gulp.dest("static/js"))
         .pipe(hash.manifest("hash.json"))
         .pipe(gulp.dest("data/js"))
-})
+
+    done();
+
+});
 
 // Watch asset folder for changes
-gulp.task("watch", ["scss", "images", "js"], function () {
-    gulp.watch("src/scss/**/*", ["scss"])
-    gulp.watch("src/img/**/*", ["images"])
-    gulp.watch("src/js/**/*", ["js"])
-})
+gulp.task("watch", gulp.series(gulp.parallel('scss', 'images','js'), function (done) {
+    gulp.watch("src/scss/**/*", gulp.series('scss'))
+    gulp.watch("src/img/**/*", gulp.series('images'))
+    gulp.watch("src/js/**/*", gulp.series('js'))
+
+    done();
+}));
 
 // Set watch as default task
-gulp.task("default", ["watch"])
+gulp.task("default", gulp.series('watch', function (done){
+    done ();
+}));
+
+
